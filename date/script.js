2eb634353291f960f7ffd59c564ea304e18d97c0
nasalzman <nasalzman@packer.edu>
2026-04-21 17:54:09 -0400

    v1.0

--- date/script.js
+++ date/script.js
@@ -6,6 +6,6 @@ let ms = now - then;
 
 document.getElementById("years").textContent = (Math.floor(ms / 1000 / 60 / 60 / 24 / 365)).toString();
 
-document.getElementById("months").textContent = (Math.floor(ms / 1000 / 60 / 24 / 30)).toString();
+document.getElementById("months").textContent = (Math.floor(ms / 1000 / 60 / 60 / 24 / 30)).toString();
 
-document.getElementById("days").textContent = (Math.floor(ms / 1000 / 60 / 24)).toString();
+document.getElementById("days").textContent = (Math.floor(ms / 1000 / 60 / 60 / 24)).toString();
